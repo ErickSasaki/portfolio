5fb71cccf557355dfbe78b456c4b33839ba4578d
Erick Batista <erick_batista07@yahoo.com>
2024-01-06 15:54:58 -0300

    feat: added github and linkedin links

--- src/content/introduction/Introduction.tsx
+++ src/content/introduction/Introduction.tsx
@@ -1,5 +1,6 @@
 import Avatar from '../../components/Avatar/Avatar';
 import './Introduction.scss';
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5"
 
 function Introduction() {
   return (
@@ -14,6 +15,16 @@ function Introduction() {
         <p className='personal-name animate'> Erick Batista </p>
 
         <p className='title animate'> I'm a Front end Developer who likes to write high quality code for high quality projects! </p>
+      
+        <p className='icons animate'>
+          <a href="https://github.com/ErickSasaki" target='blank'>
+            <IoLogoGithub />
+          </a>
+
+          <a href="https://www.linkedin.com/in/erick-batista-da-silva-developer/" target='blank'>
+            <IoLogoLinkedin />
+          </a>
+        </p>
       </div>
     </div>
   );
