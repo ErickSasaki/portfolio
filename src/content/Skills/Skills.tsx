17bd8a2f781a1aaa4b73253b8b5521cf45922427
Erick Batista <erick_batista07@yahoo.com>
2024-01-30 14:12:13 -0300

    feat: update skill icons

--- src/content/Skills/Skills.tsx
+++ src/content/Skills/Skills.tsx
@@ -1,11 +1,18 @@
 import { ReactNode } from 'react';
-import Divider from '../../components/Divider/Divider';
 import Translator from '../../components/Translator/Translator';
 import './Skills.scss';
-import { IoLogoAngular } from "react-icons/io5";
+import { IoLogoIonic, IoLogoReact, IoLogoSass } from "react-icons/io5";
 import { TranslationIds } from '../../consts/translation';
 import Card from '../../components/Card/Card';
 import AngularIcon from '../../assets/icons/AngularIcon';
+import Html5Icon from '../../assets/icons/Html5Icon';
+import Css3Icon from '../../assets/icons/Css3Icon';
+import JsIcon from '../../assets/icons/JsIcon';
+import TsIcon from '../../assets/icons/TsIcon';
+import JestIcon from '../../assets/icons/JestIcon';
+import JasmineIcon from '../../assets/icons/JasmineIcon';
+import CypressIcon from '../../assets/icons/CypressIcon';
+import { ImGit } from "react-icons/im";
 
 interface SkillInfo {
     name: string;
@@ -18,26 +25,24 @@ function Skills() {
     const skillList: SkillList = {
         main: [
             { name: 'Angular', icon: <AngularIcon /> },
-            { name: 'React', icon: <IoLogoAngular /> },
-            { name: 'React Native', icon: <IoLogoAngular /> },
-            { name: 'Ionic', icon: <IoLogoAngular /> },
-        ],
-        tests: [
-            { name: 'Jest', icon: <IoLogoAngular /> },
-            { name: 'Jasmine', icon: <IoLogoAngular /> },
-            { name: 'Karma', icon: <IoLogoAngular /> },
-            { name: 'Cypress', icon: <IoLogoAngular /> },
+            { name: 'React', icon: <IoLogoReact color="#149eca" /> },
+            { name: 'React Native', icon: <IoLogoReact color="#149eca" /> },
+            { name: 'Ionic', icon: <IoLogoIonic color="#176BFF" /> },
         ],
         base: [
-            { name: 'HTML', icon: <IoLogoAngular /> },
-            { name: 'CSS', icon: <IoLogoAngular /> },
-            { name: 'Scss', icon: <IoLogoAngular /> },
-            { name: 'Javascript', icon: <IoLogoAngular /> },
-            { name: 'Typescript', icon: <IoLogoAngular /> },
+            { name: 'HTML', icon: <Html5Icon /> },
+            { name: 'CSS', icon: <Css3Icon /> },
+            { name: 'SASS', icon: <IoLogoSass color="F06292" /> },
+            { name: 'Javascript', icon: <JsIcon /> },
+            { name: 'Typescript', icon: <TsIcon /> },
+        ],
+        tests: [
+            { name: 'Jest', icon: <JestIcon style={{ height: 40 }} /> },
+            { name: 'Jasmine', icon: <JasmineIcon style={{ height: 40 }} /> },
+            { name: 'Cypress', icon: <CypressIcon style={{ height: 40 }} /> },
         ],
         others: [
-            { name: 'Git', icon: <IoLogoAngular /> },
-            { name: 'Scrum', icon: <IoLogoAngular /> },
+            { name: 'Git', icon: <ImGit color="#de4c36"/> },
         ],
     };
 
